294580e99b84d11f01267a697694839ea380b58c
MattSteinkamp <mdsteinkamp@gmail.com>
2024-06-27 14:27:57 -0700

    added initial react app.js code to fetch all books, not working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from 'react'
 import '../App.css';
 
 export default function App() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8000/books")
+    .then(resp => resp.json())
+    .then(books => setBooks(books))
+  }, [])
+
+  console.log(books)
+
   return (
     <div className="App">
       <header className="App-header">
