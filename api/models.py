cebe7945bfc312100ca1f17bc76188edd42960b6
MattSteinkamp <mdsteinkamp@gmail.com>
2024-06-26 17:22:41 -0700

    First commit

---
+++ api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    date_added = models.DateField(auto_now_add=True)
+    characterBook = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
