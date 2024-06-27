a84abb9d9bfb68766fb213428c9bc2957202efda
MattSteinkamp <mdsteinkamp@gmail.com>
2024-06-27 13:10:57 -0700

    installed django-cors-headers and added to settings.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 from .models import Book, Character
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "date_added")
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ("name", "character_book")
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Character, CharacterAdmin)
