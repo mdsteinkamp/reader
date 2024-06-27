1e38e7d20e87db578d22862471616a9c32492a9a
MattSteinkamp <mdsteinkamp@gmail.com>
2024-06-27 14:11:38 -0700

    created Book & Character views in views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BookSerializer, CharacterSerializer
+from .models import Book, Character
 
-# Create your views here.
+class BookView(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+class CharacterView(viewsets.ModelViewSet):
+    serializer_class = CharacterSerializer
+    queryset = Character.objects.all()
