a61d8e16506daa5b15c2648299c30a3f6e5af6e9
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-06-26 20:44:51 -0700

    components folder created in client/src & relocated App.js & imports

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Lets go!
+        </p>
+
+      </header>
+    </div>
+  );
+}
