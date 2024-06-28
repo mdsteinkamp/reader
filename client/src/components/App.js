637d87924de1fbaaec5fd71f6dd4d4f9d9f4bb7f
MattSteinkamp <mdsteinkamp@gmail.com>
2024-06-27 19:15:43 -0700

    useffect fetch now in try/await in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,10 +4,23 @@ import '../App.css';
 export default function App() {
   const [books, setBooks] = useState([])
 
+  // useEffect(() => {
+  //   fetch("http://127.0.0.1:8000//api/books")
+  //   .then(resp => resp.json())
+  //   .then(books => setBooks(books))
+  // }, [])
+
   useEffect(() => {
-    fetch("http://127.0.0.1:8000//api/books")
-    .then(resp => resp.json())
-    .then(books => setBooks(books))
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000//api/books")
+        const books = await response.json()
+        setBooks(books)
+      } catch (err) {
+        console.log('Error occured when fetching books');
+      }
+    }
+    fetchBooks()
   }, [])
 
   console.log(books)
