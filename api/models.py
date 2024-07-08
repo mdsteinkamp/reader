4c89e8bc15adb842b1ddefbd1bc24debe54c834c
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-07-07 19:20:21 -0700

    created User model & related foreign key in Book model

--- api/models.py
+++ api/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=25)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.username
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     date_added = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self) -> str:
         return self.title
