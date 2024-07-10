3faf9eed95d8a10cdc897890c04fe6dbc83a82aa
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-07-09 21:45:06 -0700

    corrected project urls.py

--- reader/api/models.py
+++ reader/api/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=25)
-    date_added = models.DateField(auto_now_add=True)
+# class User(models.Model):
+#     username = models.CharField(max_length=25)
+#     date_added = models.DateField(auto_now_add=True)
 
-    def __str__(self) -> str:
-        return self.username
+#     def __str__(self) -> str:
+#         return self.username
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    date_added = models.DateField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+# class Book(models.Model):
+#     title = models.CharField(max_length=100)
+#     date_added = models.DateField(auto_now_add=True)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
-        return self.title
+#     def __str__(self) -> str:
+#         return self.title
 
-class Character(models.Model):
-    name = models.CharField(max_length=100)
-    date_added = models.DateField(auto_now_add=True)
-    character_book = models.ForeignKey(Book, on_delete=models.CASCADE)
+# class Character(models.Model):
+#     name = models.CharField(max_length=100)
+#     date_added = models.DateField(auto_now_add=True)
+#     character_book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
-        return self.name
+#     def __str__(self) -> str:
+#         return self.name
 
---
+++ reader/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from api import User, Book, Character
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username")
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title")
+        user = ("user")
+
+class CharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = ("id", "name", "character_book")
